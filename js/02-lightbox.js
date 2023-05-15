c89b850ff38dd7db4965d4a7022ef2d6711528e3
Lytvynenko-Oleksandr <dp180889los@gmail.com>
2023-05-15 22:23:58 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryItem = (item) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+};
+
+const renderGallery = (items) => {
+  const galleryItems = items.map((item) => createGalleryItem(item));
+  galleryList.append(...galleryItems);
+};
+
+renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
